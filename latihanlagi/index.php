a4f5779d1ea8c321d5922f9680e61bf00dbb7cfd
HarisMI40 <86542926+HarisMI40@users.noreply.github.com>
2022-08-25 21:56:35 +0700

    fix bug --haris

--- latihanlagi/index.php
+++ latihanlagi/index.php
@@ -4,13 +4,7 @@ $query =$database->query("select * from siswa");
 while($data=$query->fetch()):?>
 
 <p>
-    <a href="update.php?id=<? =$data['nama'];?>"
-        style="color:black;background:white;padding:2px;border-radius 3px">
-        <?= $data['nama'] ?>
-
-    </a>
-    <a href="delete.php?id=<? =$data['id'];?>"
-    style="color:white;background:red;padding:2px;border-radius 3px">Hapus</a>
-
+    <a href="update.php?id=<?= $data['nama']; ?>" style="color:black;background:white;padding:2px;border-radius 3px"> <?= $data['nama'] ?></a>
+    <a href="delete.php?id=<?= $data['id']; ?>" style="color:white;background:red;padding:2px;border-radius 3px">Hapus</a>
 </p>
 <?php endwhile ?>
